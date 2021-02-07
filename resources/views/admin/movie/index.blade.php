75e23be86af2df5e7e5b5f69a724400cff9de5e0
maki ishiguro <zhenjishihei61@outlook.jp>
2021-02-07 07:42:46 +0000

    モデルの作成

---
+++ resources/views/admin/movie/index.blade.php
@@ -0,0 +1,6 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+{{-- admin.blade.phpの＠yield（'title')に'映画の登録画面'を埋め込む --}}
+@section('title','映画の検索画面')
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
